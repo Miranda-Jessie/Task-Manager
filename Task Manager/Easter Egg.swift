2a4d79f03f3b232e049b0df935847e9f12c4fa15
Miranda Jessie <mirandajessie@MacBook-Pro-27.local>
2018-09-24 12:56:09 -0500

    Added a sleep timer to Easter Egg.

--- Task Manager/Easter Egg.swift
+++ Task Manager/Easter Egg.swift
@@ -12,7 +12,12 @@ func easterEgg() {
     //If the user puts in number 9 as a string then it will print out this "hidden" function
     let userInputForEasterEgg: String = ""
     if userInputForEasterEgg == "" {
-        print("\nHey! What are you doing here? Since you wanted to go snooping I guess I only have one option left to do... BOOM! You are dead. I hope you are happy. Maybe next time you wont try and find something to get into. Have a good rest of your day ;) ")
+        sleep (3)
+        print("\nHey! What are you doing here? Since you wanted to go snooping I guess I only have one option left to do...")
+        sleep (3)
+        print("\nBOOM! You are dead. I hope you are happy. Maybe next time you wont try and find something to get into. Have a good rest of your day 😉")
+        sleep (3)
+        
     }
     //This calls the function to be printed out.
     print(userInputForEasterEgg)
